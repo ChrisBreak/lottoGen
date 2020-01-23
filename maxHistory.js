a4f3340dc1164f469ff7b7eaa9c82d817def4cf8
Cristhian <csotelo88@hotmail.com>
2020-01-23 12:43:34 -0700

    all histories updated

--- maxHistory.js
+++ maxHistory.js
@@ -1,4 +1,5 @@
 let maxHistory = [
+  {date:"January 21 2020", main:"7 11 16 21 31 36 44", bonus:"46"},
   {date:"January 17 2020", main:"5 6 36 39 40 46 48", bonus:"34"},
   {date:"January 14 2020", main:"5 16 19 23 30 37 48", bonus:"44"},
   {date:"January 10 2020", main:"10 13 14 24 28 47 48", bonus:"12"},
